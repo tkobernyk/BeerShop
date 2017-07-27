f3fb381f81779e99a917428eb4912aae468899fd
tkobernyk <taras.kobernyk@gmail.com>
2017-07-27 21:24:20 +0300

    Added Solution for OAuth 2.0

---
+++ BeerShop.Api.OAuth2/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+
+
+[assembly: OwinStartup(typeof(BeerShop.Api.OAuth2.Startup))]
+
+namespace BeerShop.Api.OAuth2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWebApi(WebApiConfig.Register());
+        }
+    }
+}
